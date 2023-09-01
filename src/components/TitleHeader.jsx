2a21798133c3812a7ad5b8c1fbe3904a21d6466d
justdilyorbek <142020869+justdilyorbek@users.noreply.github.com>
2023-09-01 11:27:28 +0500

    first commit

--- src/components/TitleHeader.jsx
+++ src/components/TitleHeader.jsx
@@ -6,9 +6,9 @@ const TitleHeader = ({title}) => {
         <div className="flex items-center justify-between">
         <h1 className="text-left font-semibold text-3xl text-normalBlack leading-[42px]">
           {title}
-        </h1>
-        <span className="flex items-center text-lightOrange text-sm font-medium hover:text-orange-300">
-          <a href="#" className="leading-[51px] mr-1">View all  </a><FaChevronRight />
+        </h1> 
+        <span className="flex w-full items-center text-lightOrange text-sm font-medium hover:text-orange-300">
+          <a href="#" className="leading-[51px] mr-1 ml-auto">View all  </a><FaChevronRight />
         </span>
       </div>
     </div>
